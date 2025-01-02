31466f5dcc192fdaf679b5843be7e63ec84a9fab
Joe Anderson <and10287@umn.edu>
2025-01-01 20:19:07 -0600

    created WINGS project page

---
+++ src/app/projects/wings/page.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@/once-ui/components";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "WINGS | Joe Anderson"
+}
+
+const Wings = () => {
+    return (<>
+        <p>WINGS page</p>
+    </>)
+}
+
+export default Wings;
